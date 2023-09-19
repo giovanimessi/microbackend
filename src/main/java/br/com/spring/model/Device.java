6501550d3ae0c25ac05cf01df3317c3655978f97
giovani <giovanimouradev@gmail.com>
2023-09-19 20:25:40 -0300

    model device

---
+++ src/main/java/br/com/spring/model/Device.java
@@ -0,0 +1,36 @@
+package br.com.spring.model;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_devices")
+public class Device {
+	
+	//tipo de dispositivo
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	
+	@Column(name = "name",nullable = false, length = 100)
+	private String name;
+	
+	@Column(name = "description", nullable = false, length = 100)
+	private String description;
+	
+	@Column(name = "price",nullable = false)
+	private Double price;
+	
+	@Column(name = "create_time", nullable = false)
+	private LocalDateTime createTime;
+	
+	
+
+}
