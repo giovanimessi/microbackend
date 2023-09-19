1ea4de1283d0c7a9f5f7423211716c29ded5ed8a
giovani <giovanimouradev@gmail.com>
2023-09-18 22:13:14 -0300

    init project

---
+++ src/main/java/br/com/spring/model/Role.java
@@ -0,0 +1,7 @@
+package br.com.spring.model;
+
+public enum Role {
+	USER,
+	ADMIN
+
+}
---
+++ src/main/java/br/com/spring/model/User.java
@@ -0,0 +1,55 @@
+package br.com.spring.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+import javax.persistence.Id;
+
+@Data
+
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+
+	private Long Id;
+	
+	@Column(name = "username", unique=true, nullable = false, length=100)
+	private String username;
+	
+	
+	@Column(name = "password",  nullable = false, length=100)
+	private String password;
+	
+	@Column(name = "name",  nullable = false, length=100)
+	private String name;
+	
+
+	
+	@Column(name = "create_time",nullable = false)
+	private LocalDateTime createTime;
+	
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false)
+	private Role role;
+	
+	
+}
