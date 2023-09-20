d8fa1e6017e94359707484ca9cf1e8a26fe457c7
giovani <giovanimouradev@gmail.com>
2023-09-20 20:44:11 -0300

    repository user

---
+++ src/main/java/br/com/spring/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package br.com.spring.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.spring.model.Role;
+import br.com.spring.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User>findByUsername(String username);
+	
+	
+	@Query
+	void updateUserRole(@Param("username")String username, @Param("role")Role role);
+
+}
